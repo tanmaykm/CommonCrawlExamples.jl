af8746079cf68319289dd346cf3f76d7082021e1
tan <tanmaykm@gmail.com>
2015-05-27 12:43:17 +0530

    searcher optionally returns url instead of docid

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # CommonCrawlExamples
 
-[![Build Status](https://travis-ci.org/tanmaykm/CommonCrawlExamples.jl.svg?branch=master)](https://travis-ci.org/tanmaykm/CommonCrawlExamples.jl)
+These are some toy examples using CommonCrawl.jl, suitable for demonstratons.
+
+## Simple Distributed Indexing and Searching
+
+The index simply represents documents where each word in the vocabulary appears, without any relevance score.
 
 - Make sure you have your AWS access keys setup in the file `~/.awssecret` (ref: https://github.com/amitmurthy/AWS.jl)
 - `Pkg.clone("https://github.com/tanmaykm/CommonCrawlExamples.jl.git")`
 - `using CommonCrawlExamples`
-- `setup_folders(clean=true) # use true to discard cached files and indexes and rebuild afresh`
+- `setup_folders(true) # use true to discard cached files and indexes and rebuild afresh, false to reuse`
 - `create_index(5) # index 5 archives`
+- `search_index("search terms") # search for any of the words`
--- src/searcher.jl
+++ src/searcher.jl
@@ -2,13 +2,26 @@
 # Searching
 ##########################################################
 
-function search_part_idx(file, terms::Array)
+function search_part_idx(file, terms::Array, mapto_url=true, limit=10)
     part_idx = cached_get(file, ()->as_deserialized(file))
     results = IntSet()
     for term in terms
         union!(results, get(part_idx, term, []))
     end
-    results
+    if limit > 0
+        while length(results) > limit
+            pop!(results)
+        end
+    end
+
+    if mapto_url
+        id_map_file = joinpath(id_to_doc_location, basename(file))
+        id_to_doc = cached_get(id_map_file, ()->as_deserialized(openable(id_map_file)))
+        result_docs = map(id->get(id_to_doc, id, ""), results)
+        return Set(filter(x->!isempty(x), result_docs))
+    else
+        return Set(results)
+    end
 end
 
 function search_index(terms::String)
@@ -17,19 +30,19 @@ function search_index(terms::String)
     terms = tokens(td)
     terms = filter(tok->!isempty(tok), terms)
 
+    # The Blocks library provides many ways to divide work among processors.
+    # Here, we call it on the part_idx_location directory, which tells it to
+    # divide up the files in that directory.
+    # The call localpart(master_idx) returns a list of files to be handled
+    # by the calling processor.
     master_idx = cached_get(part_idx_location, ()->Block(openable(part_idx_location), false, 2))
 
-    result_doc_ids = @parallel union for i in 1:nworkers()
+    result_docs = @parallel union for i in 1:nworkers()
         local_files = {}
         for b in localpart(master_idx)
             append!(local_files, b)
         end
         reduce(union, map(file->search_part_idx(file, terms), local_files))
     end
-
-    println("doc ids: $result_doc_ids")
-    # map the document ids to file names 
-    #id_to_doc = cached_get(id_to_doc_location, ()->as_deserialized(openable(id_to_doc_location)))
-    #result_docs = map(id->get(id_to_doc, id, ""), result_doc_ids)
-    #filter(x->!isempty(x), result_docs)
+    result_docs
 end
