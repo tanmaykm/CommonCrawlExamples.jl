1aec29ba195043dba7dee73d87b82b4193717581
tan <tanmaykm@gmail.com>
2015-05-27 15:37:47 +0530

    fix for reduce not being allowed on empty arrays
    also more options for searcher

--- src/searcher.jl
+++ src/searcher.jl
@@ -2,29 +2,39 @@
 # Searching
 ##########################################################
 
-function search_part_idx(file, terms::Array, mapto_url=true, limit=10)
-    part_idx = cached_get(file, ()->as_deserialized(file))
-    results = IntSet()
-    for term in terms
-        union!(results, get(part_idx, term, []))
-    end
+function search_part_idx(file, terms::Array, mapto_url::Bool, limit::Int)
+    results = search_part_idx(file, terms, mapto_url)
     if limit > 0
         while length(results) > limit
             pop!(results)
         end
     end
+    results
+end
+
+function search_part_idx(file, terms::Array, mapto_url::Bool)
+    results = search_part_idx(file, terms)
 
     if mapto_url
         id_map_file = joinpath(id_to_doc_location, basename(file))
         id_to_doc = cached_get(id_map_file, ()->as_deserialized(openable(id_map_file)))
         result_docs = map(id->get(id_to_doc, id, ""), results)
         return Set(filter(x->!isempty(x), result_docs))
-    else
+    else 
         return Set(results)
     end
 end
 
-function search_index(terms::String)
+function search_part_idx(file, terms::Array)
+    part_idx = cached_get(file, ()->as_deserialized(file))
+    results = IntSet()
+    for term in terms
+        union!(results, get(part_idx, term, []))
+    end
+    results
+end
+
+function search_index(terms::String; mapto_url::Bool=true, limit::Int=10)
     sd = StringDocument(terms)
     td = preprocess(sd)
     terms = tokens(td)
@@ -37,12 +47,18 @@ function search_index(terms::String)
     # by the calling processor.
     master_idx = cached_get(part_idx_location, ()->Block(openable(part_idx_location), false, 2))
 
-    result_docs = @parallel union for i in 1:nworkers()
+    results = @parallel union for i in 1:nworkers()
         local_files = {}
         for b in localpart(master_idx)
             append!(local_files, b)
         end
-        reduce(union, map(file->search_part_idx(file, terms), local_files))
+        search_results = map(file->search_part_idx(file, terms, mapto_url, limit), local_files)
+        isempty(search_results) ? {} : reduce(union, search_results)
     end
-    result_docs
+    if limit > 0
+        while length(results) > limit
+            pop!(results)
+        end
+    end
+    collect(results)
 end
